144ca8c1d9eb539d77663f6249f5f52de1baea09
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2022-02-21 18:01:10 +0900

    Add files via upload

---
+++ Scripts/Manager_Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public DataController DataController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickRestart()
+    {
+        DataController.SaveGameData();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        DataController.LoadGameData();
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
